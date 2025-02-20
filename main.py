5fc081c3eb4471d0ab033440a3680021ead7b26c
Vadim Rubashko <v_rubashko@lesta.group>
2025-02-20 18:17:59 +0300

    20.02

--- main.py
+++ main.py
@@ -1,16 +1,132 @@
-# This is a sample Python script.
+from random import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+TOTAL_POINTS = 10
+SHIP_TYPES = {1: "Броненосец",
+              2: "Торпедный катер",
+              3: "Противокорабельная лодка"}
+
+def get_rules():
+    print(f"Правила игры:\n"
+          f"Игроку необходимо указать название, тип и ТТХ 2-х короблей, которые будут участвовать в бою.\n"
+          f"Суммарно для всех ТТХ (хп, урон, скорость) доступно {TOTAL_POINTS} очков (значение не может быть = 0)\n"
+          f"Очки прочности вычисляются по формуле: HP = hp.points x 100\n"
+          f"Очки урона вычисляются по формуле: DMG = dmg.points x 10\n"
+          f"Очки скорости вычисляются по формуле: SPEED = speed.points\n"
+          f"Урон наносится DMG +/-10%\n"          
+          f"Очки скорости влияют на вероятность коробля увернуться от получения урона: шанс_уворота = SPEED х 0,05\n"
+          f"Каждый тип корабля обладает своими уникальными способностями:\n"
+          f"Броненосец - получает в два раза меньше урона\n"
+          f"Торпедный катер - имеет удвоенную скорость (SPEED х 2)\n"
+          f"Противокорабельная лодка - имеет повышенный урон (DMG x 1.5)\n"
+          f"А знаете, где ещё есть танки, авиация и корабли? Конечно же, в...")
+
+def input_digit():
+    while True:
+        s = input()
+        try:
+            return int(s)
+        except ValueError:
+            print('Введен неправильный тип. Ожидалось целое число.')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class BattleShip:
+    def __init__(self, name , ship_type, hp, damage, speed):
+        self._name = name
+        self.ship_type = ship_type
+        self.hp = hp
+        self.damage = damage
+        self.speed = speed
+
+    @property
+    def ship_type(self):
+        return self._ship_type
+
+    @ship_type.setter
+    def ship_type(self, ship_type):
+        types = SHIP_TYPES
+        self._ship_type = types[ship_type]
+
+    @property
+    def hp(self):
+        return self._hp
+
+    @hp.setter
+    def hp(self, hp):
+        self._hp = hp * 100
+
+    @property
+    def damage(self):
+        return self._damage
+
+    @damage.setter
+    def damage(self, damage):
+        self._damage = damage * 15 if self.ship_type == 3 else damage * 10
+
+    @property
+    def speed(self):
+        return self._speed
+
+    @speed.setter
+    def speed(self, speed):
+        self._speed = speed * 2 if self.ship_type == 2 else speed
+
+    def get_info(self):
+        print(f"{'Корабль':.<17}{self._name}\n"
+              f"{'Тип':.<17}{self._ship_type}\n"
+              f"{'Прочность':.<17}{self._hp}\n"
+              f"{'Средний урон':.<17}{self._damage}\n"
+              f"{'Скорость':.<17}{self._speed}")
+
+    def deal_damage(self):
+        k = random() * (11 - 9) + 9
+        return round(self._damage * round(k, 1) * 0.1)
+
+    def take_damage(self, damage):
+        print(self._ship_type)
+        self._hp -= round(damage / 2) if self._ship_type == "Броненосец" else damage
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def ship_input():
+    name = input("Название корабля:\n")
+    print(f"Выберите тип коробля от 1 до {len(SHIP_TYPES)}, где:")
+    print(*[f"{key} - {value}" for key, value in SHIP_TYPES.items()], sep="\n")
+    ship_type = input_digit()
+    while ship_type not in SHIP_TYPES:
+        print("Указанный тип отсутствует, повторите ввод")
+        print(*[f"{key} - {value}" for key, value in SHIP_TYPES.items()], sep="\n")
+        ship_type = input_digit()
+    print(f"Укажите ТТХ корабля (доступно {TOTAL_POINTS} очков)")
+    print(f"Очки прочности: число от 1 до {TOTAL_POINTS - 2} (итоговое ХП = очки прочности х 10):")
+    hp = input_digit()
+    while hp < 1 or hp > TOTAL_POINTS - 2:
+        print(f"Указаное количество очков прочности не попадает в диапазон от 1 до {TOTAL_POINTS - 2}")
+        hp = input_digit()
+    print(f"Очки урона: число от 1 до {TOTAL_POINTS - hp - 1}")
+    damage = input_digit()
+    while damage < 1 or damage > TOTAL_POINTS - hp - 1:
+        print(f"Указаное количество очков урона не попадает в диапазон от 1 до {TOTAL_POINTS - hp - 1}")
+        damage = input_digit()
+    print(f"Очки скорости:")
+    speed = TOTAL_POINTS - hp - damage
+    print(speed)
+    return name, ship_type, hp, damage, speed
+
+# print("<---BATTLE SHIP, ver. 1.0--->")
+# start = input("Нажмите Enter, чтобы начать игру (для получения правил игры введите info)\n")
+# if start == "info":
+#     get_rules()
+#     input("Нажмите Enter, чтобы начать игру")
+#
+# print(f"{"":-<32}")
+# print(f"Введите данные первого корабля")
+input_1 = ship_input()
+battle_ship_1 = BattleShip(input_1[0], input_1[1], input_1[2], input_1[3], input_1[4])
+battle_ship_1.get_info()
+# print(f"{"":-<32}")
+# input("нажмите Enter, чтобы продолжить")
+# print(f"{"":-<32}")
+# print("Введите данные второго корабля")
+# input_2 = ship_input()
+# battle_ship_2 = BattleShip(input_2[0], input_2[1], input_2[2], input_2[3], input_2[4])
+# battle_ship_2.get_info()
